0eeccf6f86f2bc4a093ebbb213bdb07841ff4eb4
ktrskr <150217076+ktrskr@users.noreply.github.com>
2024-03-25 14:02:47 +0530

    Add files via upload

---
+++ health.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.get('/health', (req, res) => {
+    res.send('OK');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
